57bfdfcb19e10aebbe5b98ce2ae07e187465c4c6
MiriamLongoyapus <longoyapusmiriam@gmail.com>
2023-02-02 20:06:41 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val name = "Ekal"
+    println(name)
+    val age = 30
+    println(age)
+    val phoneNumber = "0740075989"
+    println(phoneNumber)
+val weight = 70
+    println(weight)
+    val CitizenKenyan = true
+    println(CitizenKenyan)
+}
